892daee7709cd9efd021ef8b96bff65bbcbed432
PigeonKur <ggnn3974@gmail.com>
2025-06-11 22:11:48 +0300

    MainScreen 80% finished

--- MoneyFlowClient/MoneyFlowClient.Client/Models/UserRegisterModel.cs
+++ MoneyFlowClient/MoneyFlowClient.Client/Models/UserRegisterModel.cs
@@ -2,10 +2,9 @@
 {
     public class UserRegisterModel
     {
-        public int UserId { get; set; } = 8;
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
-        public int RoleId { get; set; } = 1; // например дефолтная роль
+        public int RoleId { get; set; } = 2; // например дефолтная роль
     }
 }
